c34997d5ce1a9fcd659bba5bd5827ba250138d9c
RajashriDharane <rajashridharane@gmail.com>
2024-10-07 22:28:51 -0700

    Add files via upload

---
+++ ArrayLinked.java
@@ -0,0 +1,20 @@
+package collection;
+import java.util.LinkedList;
+
+public class ArrayLinked {
+	
+
+	 
+	    public static void main(String[] args){
+	        LinkedList<String> obj = new LinkedList<String>();
+	        obj.add("ajay");
+	        obj.add("pandu");
+	        System.out.println(obj);
+	        obj.add("pravin");
+	        System.out.println(obj);
+	        obj.addFirst("Vishal");
+	        System.out.println(obj);
+	    }
+	}
+
+
